75f9235146d8cfb1548a18ef0ca256b927d385c7
tleonardo6240 <tomasdleonardo@outlook.com>
2026-03-02 12:38:45 -0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
+
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+};
+
+export default withMDX(nextConfig);
